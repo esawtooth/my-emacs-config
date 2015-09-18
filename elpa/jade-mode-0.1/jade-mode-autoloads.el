a1cdcb4ed95a68b24837a23b00bd4d212dac4c5c
Rohit.Jain <rohit.jain@guavus.com>
2015-09-18 12:10:29 +0530

    Deleted old stuff

--- elpa/jade-mode-0.1/jade-mode-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; jade-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("jade-mode.el") (21099 22330 10175 755000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; jade-mode-autoloads.el ends here
--- elpa/org-20130919/org-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org" "20130919" "Outline-based notes management and organizer" 'nil)
